1212b3f922c32154d093777f4d1ab996ea2fc8cb
Viswa V <viswamalar2004@gmail.com>
2025-10-17 16:52:39 +0530

    Migrated to postgresSQL

--- voice_assistant/settings.py
+++ voice_assistant/settings.py
@@ -24,10 +24,15 @@ ROOT_URLCONF = 'voice_assistant.urls'
 WSGI_APPLICATION = 'voice_assistant.wsgi.application'
 
 # Database
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST', 'localhost'),
+        'PORT': os.getenv('DB_PORT', '5432'),
     }
 }
 
